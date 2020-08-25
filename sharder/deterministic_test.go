478d413d57d6237513c652db7008be1e7125e772
Alaina Valenzuela <asdvalenzuela@gmail.com>
2020-08-25 12:56:18 -0700

    update deterministic sharder test with new peer management

--- sharder/deterministic_test.go
+++ sharder/deterministic_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/honeycombio/samproxy/config"
+	"github.com/honeycombio/samproxy/internal/peer"
 	"github.com/honeycombio/samproxy/logger"
 	"github.com/stretchr/testify/assert"
 )
@@ -22,10 +23,14 @@ func TestWhichShard(t *testing.T) {
 	config := &config.MockConfig{
 		GetPeerListenAddrVal: selfAddr,
 		GetPeersVal:          peers,
+		PeerManagementType:   "file",
 	}
+	filePeers, err := peer.NewPeers(config)
+	assert.Equal(t, nil, err)
 	sharder := DeterministicSharder{
 		Config: config,
 		Logger: &logger.NullLogger{},
+		Peers:  filePeers,
 	}
 
 	assert.NoError(t, sharder.Start(),
